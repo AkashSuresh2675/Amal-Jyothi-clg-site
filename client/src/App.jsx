8f1739bb3dacb9980e63160c746d20c3675f9bcc
Akhi1Babu <akhilb732@gmail.com>
2026-02-11 10:17:30 +0530

    feat(client): implement Placements page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Faculty from './pages/Faculty';
 import Admissions from './pages/Admissions';
 import CampusLife from './pages/CampusLife';
 import Contact from './pages/Contact';
+import Placements from './pages/Placements';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/faculty" element={<Faculty />} />
         <Route path="/admissions" element={<Admissions />} />
         <Route path="/campus-life" element={<CampusLife />} />
+        <Route path="/placements" element={<Placements />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Route>
